96f03d1afecfc24623eee37e788ed27355c0cd30
jie lian <imjeee@gmail.com>
2011-04-29 18:10:28 -0500

    implemented brute force

--- src/StrMatch.java
+++ src/StrMatch.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StrMatch {
+	
 	public static void main(String[] args) {
 		if (args.length != 3) {
 			throw new IllegalArgumentException(
@@ -19,6 +20,7 @@ public class StrMatch {
 		ArrayList<String> eachPattern = new ArrayList<String>();
 		
 		try {
+			//read pattern
 			p = new File(pattern);
 			Scanner scan = new Scanner(p).useDelimiter("&");
 			while (scan.hasNext()) {
@@ -28,26 +30,72 @@ public class StrMatch {
 					eachPattern.add(thisPattern);
 				}
 			}
+			
+			
+			
+			
 		} catch (IOException e) {
 			System.out.println("could not read pattern file: " + args[0]);
 		}
 
 	}
-	
-	/*
-	 * String input = "1 fish 2 fish red fish blue fish";
-     Scanner s = new Scanner(input);
-     s.findInLine("(\\d+) fish (\\d+) fish (\\w+) fish (\\w+)");
-     MatchResult result = s.match();
-     for (int i=1; i<=result.groupCount(); i++)
-         System.out.println(result.group(i));
-     s.close(); 
-	 */
 
+
+	//Brute Force Algorithm
+	public static int bruteForce(String pattern, String source){
+		
+		for(int i = 0; i < source.length() - pattern.length(); i++){
+			int tempi = i;
+			boolean match = true;
+			for(int j = 0; j < pattern.length() & match; j++, tempi++){
+				if(source.charAt(tempi) != pattern.charAt(j)){
+					match = false;
+				}
+			}
+			if(match){
+				return 1;
+			}
+		}
+		return -1;
+	}
+	
+	
 	// Rabin-Karp Algorithm
+	public static int rabinKarp(String pattern, String source){
+		int patternHValue = pattern.hashCode();
+		
+		return -1;
+	}
+	
 
 	// Knuth-Morris-Pratt Algorithm
+	public static int knuthMorrisPratt(String pattern, String source){
+		
+		int m = 0;
+		int i = 0;
+		
+		while(m+i < source.length()){
+			if(pattern.charAt(i) == source.charAt(m+i)){
+				if(i == pattern.length()-1){
+					return m;
+				}
+				i++;
+			} else {
+				m = m + i - partialMatch(i);
+				if(partialMatch(i) > -1){
+					i = partialMatch(i);
+				} else {
+					i = 0;
+				}
+			}
+		}
+		
+		return -1;
+	}
 
 	// Boyer-Moore Algorithm
+	public static int boyerMoore(String pattern, String source){
+		return -1;
+	}
 
 }
