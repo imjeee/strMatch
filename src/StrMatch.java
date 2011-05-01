9f6597d77cf706efc68d130ee2125f99a6429cba
jie lian <imjeee@gmail.com>
2011-05-01 01:25:32 -0500

    boyer moore works!!!!!

--- src/StrMatch.java
+++ src/StrMatch.java
@@ -38,13 +38,15 @@ public class StrMatch {
 	}
 
 	// Brute Force Algorithm
-	public static int bruteForce(String pattern, LookBackStringBuffer source) throws Exception {
+	public static int bruteForce(String pattern, LookBackStringBuffer source)
+			throws Exception {
 		assert (pattern.length() > 0) : "pattern cannot be empty";
 
-		System.out.println("at bruteforce: pattern: " + pattern + "; source: " + source);
-		
-		//for (int i = 0; i < source.length() - pattern.length() + 1; i++) {
-		for (int i = 0; source.hasAvailable(i + pattern.length()); i++){
+		System.out.println("at bruteforce: pattern: " + pattern + "; source: "
+				+ source);
+
+		// for (int i = 0; i < source.length() - pattern.length() + 1; i++) {
+		for (int i = 0; source.hasAvailable(i + pattern.length()); i++) {
 			int tempi = i;
 			boolean match = true;
 			for (int j = 0; j < pattern.length() & match; j++, tempi++) {
@@ -60,149 +62,104 @@ public class StrMatch {
 		return -1;
 	}
 
-	
-	// Rabin-Karp Algorithm
-	public static int rabinKarp(String pattern, LookBackStringBuffer source) throws IOException, Exception {
-		
-		if(!source.hasAvailable(pattern.length())){
+	// Rabin-Karp Algorithm, Rolling linear sum
+	public static int rabinKarp(String pattern, LookBackStringBuffer source)
+			throws IOException, Exception {
+
+		if (!source.hasAvailable(pattern.length())) {
 			return -1;
 		}
-		
+
 		String sub = new String();
-		
-		int primeN = 47;
-		int pl = pattern.length();
-		
+
 		long patternHC = 0;
 		long sourceHC = 0;
-		
-		for(int i = 0; i < pattern.length(); i++){
-			//calculate the whole pattern hash
+
+		for (int i = 0; i < pattern.length(); i++) {
+			// calculate the whole pattern hash
 			long patternCharInt = pattern.charAt(i);
 			patternHC += patternCharInt;
-			//calculate first source has
+			// calculate first source has
 			char sourceChar = source.charAt(i);
 			long sourceCharInt = (long) sourceChar;
 			sourceHC += sourceCharInt;
-			
+
 			sub += sourceChar;
 		}
-		
-		//System.out.println("pattern: " + patternHC + " source: " + sourceHC);
-		
-		for(int i = pattern.length()-1; source.hasAvailable(i + 1); i++){
-			//System.out.println("sourceHC " + sourceHC);
-			if(patternHC == sourceHC){
-				if(bruteForce(pattern, new LookBackStringBuffer(sub)) != -1){
+
+		// System.out.println("pattern: " + patternHC + " source: " + sourceHC);
+
+		for (int i = pattern.length() - 1; source.hasAvailable(i + 1); i++) {
+			// System.out.println("sourceHC " + sourceHC);
+			if (patternHC == sourceHC) {
+				if (bruteForce(pattern, new LookBackStringBuffer(sub)) != -1) {
 					return 1;
 				}
 			}
-			//System.out.println(source.charAt(i));
-			
-			if(!source.hasAvailable(i+2)){
+			// System.out.println(source.charAt(i));
+
+			if (!source.hasAvailable(i + 2)) {
 				break;
 			}
-			
-			char next = source.charAt(i+1);
-			long nextC = (long)next;
+
+			char next = source.charAt(i + 1);
+			long nextC = (long) next;
 			sub = sub.substring(1, sub.length()) + next;
 			long prevHC = source.charAt(i - pattern.length() + 1);
 			sourceHC = sourceHC - prevHC + nextC;
-			
-			//System.out.println("prev: " + prevHC);
-			
-			
-			//System.out.println("next: " + nextC + " source: " + sourceHC);
+
+			// System.out.println("prev: " + prevHC);
+
+			// System.out.println("next: " + nextC + " source: " + sourceHC);
 		}
-		
 
 		return -1;
 	}
-	
-	/*
-	 * // Rabin-Karp Algorithm
-	public static int rabinKarp(String pattern, String source) throws IOException, Exception {
-		assert pattern.length() < source.length() : "pattern needs to be longer than source";
-		
-		String sub = new String();
-		
-		int primeN = 47;
-		int pl = pattern.length();
-		
-		long patternHC = 0;
-		long sourceHC = 0;
-		
-		for(int i = 0; i < pattern.length(); i++){
-			//calculate the whole pattern hash
-			long patternCharInt = pattern.charAt(i);
-			patternHC += patternCharInt;
-			//calculate first source has
-			char sourceChar = source.charAt(i);
-			long sourceCharInt = (long) sourceChar;
-			sourceHC += sourceCharInt;
-			
-			sub += sourceChar;
-		}
-		
-		//System.out.println("pattern: " + patternHC + " source: " + sourceHC);
-		
-		for(int i = pattern.length()-1; i < source.length(); i++){
-			//System.out.println("sourceHC " + sourceHC);
-			if(patternHC == sourceHC){
-				if(bruteForce(pattern, new LookBackStringBuffer(sub)) != -1){
-					return 1;
-				}
-			}
-			//System.out.println(source.charAt(i));
-			
-			if(i+1 >= source.length()){
-				break;
-			}
-			
-			char next = source.charAt(i+1);
-			long nextC = (long)next;
-			sub = sub.substring(1, sub.length()) + next;
-			long prevHC = source.charAt(i - pattern.length() + 1);
-			sourceHC = sourceHC - prevHC + nextC;
-			
-			//System.out.println("prev: " + prevHC);
-			
-			
-			//System.out.println("next: " + nextC + " source: " + sourceHC);
-		}
-		
 
-		return -1;
-	}*/
-	
 	/*
-	 * Fast Modular Exponentiation
+	 * // Rabin-Karp Algorithm public static int rabinKarp(String pattern,
+	 * String source) throws IOException, Exception { assert pattern.length() <
+	 * source.length() : "pattern needs to be longer than source";
+	 * 
+	 * String sub = new String();
+	 * 
+	 * int primeN = 47; int pl = pattern.length();
+	 * 
+	 * long patternHC = 0; long sourceHC = 0;
+	 * 
+	 * for(int i = 0; i < pattern.length(); i++){ //calculate the whole pattern
+	 * hash long patternCharInt = pattern.charAt(i); patternHC +=
+	 * patternCharInt; //calculate first source has char sourceChar =
+	 * source.charAt(i); long sourceCharInt = (long) sourceChar; sourceHC +=
+	 * sourceCharInt;
+	 * 
+	 * sub += sourceChar; }
+	 * 
+	 * //System.out.println("pattern: " + patternHC + " source: " + sourceHC);
+	 * 
+	 * for(int i = pattern.length()-1; i < source.length(); i++){
+	 * //System.out.println("sourceHC " + sourceHC); if(patternHC == sourceHC){
+	 * if(bruteForce(pattern, new LookBackStringBuffer(sub)) != -1){ return 1; }
+	 * } //System.out.println(source.charAt(i));
+	 * 
+	 * if(i+1 >= source.length()){ break; }
+	 * 
+	 * char next = source.charAt(i+1); long nextC = (long)next; sub =
+	 * sub.substring(1, sub.length()) + next; long prevHC = source.charAt(i -
+	 * pattern.length() + 1); sourceHC = sourceHC - prevHC + nextC;
+	 * 
+	 * //System.out.println("prev: " + prevHC);
+	 * 
+	 * 
+	 * //System.out.println("next: " + nextC + " source: " + sourceHC); }
+	 * 
+	 * 
+	 * return -1; }
 	 */
-	private static long fastExponentiation(long message, long exp, long n) {
-		//shortcut 1 if m > n
-		if(message > n) { 
-			message = message%n; 
-		}
-		
-		//shortcut 2 if e > n-1
-		if(exp > n - 1) { 
-			exp = exp%(n-1); 
-		}
-		
-		long c = 1;
-
-		while (exp != 0) {
-			if (exp % 2 != 0) {
-				c = (c * message) % n;
-			}
-			exp = exp / 2;
-			message = (message * message) % n;
-		}
-		return c;
-	}
 
 	// Knuth-Morris-Pratt Algorithm
-	public static int knuthMorrisPratt(String pattern, LookBackStringBuffer source) throws Exception {
+	public static int knuthMorrisPratt(String pattern,
+			LookBackStringBuffer source) throws Exception {
 		assert (pattern.length() > 0) : "pattern cannot be empty";
 
 		LookBackStringBuffer s = source;
@@ -258,92 +215,141 @@ public class StrMatch {
 
 		return T;
 	}
-	
-	/*
-	
+
 	// Boyer-Moore Algorithm
-	public static int boyerMoore(String pattern, String source) {
-		
+	public static int boyerMoore(String pattern, LookBackStringBuffer source) throws Exception {
+
 		int pl = pattern.length();
-		int sl = source.length();
-		
-		if(sl == 0 | pl == 0 | pl > sl){
+
+		if (pl == 0 | !source.hasAvailable(0)) {
 			return -1;
 		}
-		
-		//ALPHABET_SIZE??????????
-		int ALPHABET_SIZE = 0;
-		
-		int[] badcharacter = new int[ALPHABET_SIZE];
-		int[] goodsuffix = new int[pl + 1];
-		
+
+		int[] badcharacter = prepare_badcharacter(pattern);
+		int[] goodSuffix = prepare_goodSuffix(pattern);
+
 		int s = 0;
-		
-		while(s < sl - pl){
+		while (source.hasAvailable(s)) {
 			int j = pl;
-			while(j > 0 & pattern.charAt(j-1) == source.charAt(s+j-1)){
+			while (j > 0 && pattern.charAt(j - 1) == source.charAt(s + j - 1)) {
 				j--;
 			}
-			if(j>0){
-				//int k = badcharacter[(size_t) haystack[s+j-1]];
-				int k = badcharacter[(int) source.charAt(s-j+1)];
-				//int m;
-				int m = j-k-1;
-				if((k < j) && (m > goodsuffix[j])){
+			if (j > 0) {
+				int k = badcharacter[source.charAt(s + j - 1)];
+				int m = j - k - 1;
+				if (k < j & m > goodSuffix[j]) {
 					s += m;
 				} else {
-					s += goodsuffix[j];
+					s += goodSuffix[j];
 				}
-			} else
+			} else {
 				return 1;
+			}
 		}
-		
 		return -1;
+
 	}
-	
+
 	//computer prefix
-	private static void computer_prefix(String str, int size, int[] result){
-		int q = size;
+	private static int[] computer_prefix(String str) {
+
+		int size = str.length();
+		int[] result = new int[size];
+
 		int k = 0;
 		result[0] = 0;
-		
-		for(q = 1; q < size; q++){
-			while(k > 0 & str.charAt(k) != str.charAt(q)){
-				k = result[k-1];
+
+		for (int q = 1; q < size; q++) {
+			char ck = str.charAt(k);
+			char cq = str.charAt(q);
+			while (k > 0 && ck != cq) {
+				k = result[k - 1];
 			}
-			if(str.charAt(k) == str.charAt(q)){
+
+			if (ck == cq) {
 				k++;
 			}
+
 			result[q] = k;
 		}
+		return result;
 	}
-	
-	//prepare badcharacter heuristic
-	private static void prepareBadChar(String str, int size, int[] result){
-		int i = size;
-		
-		for(i = 0; i < result.length; i++){
+
+	//prepare bad character, bad symbol heuristic
+	private static int[] prepare_badcharacter(String str) {
+
+		int size = str.length();
+		int charE = 10000;
+		int[] result = new int[charE];
+
+		for (int i = 0; i < charE; i++) {
 			result[i] = -1;
 		}
-		
-		for(int j = 0; j < size; j++){
-			result[(int) str.charAt(i)] = i;
+
+		for (int j = 0; j < size; j++) {
+			result[(int) str.charAt(j)] = j;
 		}
+
+		return result;
 	}
-	
-	//prepare goodsuffix heuristic
-	private static void prepareGoodSuffix(String normal, int size, int[] result){
-		String left = normal;
-		//String right = 
-		
+
+	//good suffix heuristic
+	private static int[] prepare_goodSuffix(String str) {
+
+		int size = str.length();
+		int[] result = new int[str.length() + 1];
+
+		// reverse string
+		StringBuffer sb = new StringBuffer(str.length());
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sb.append(str.charAt(i));
+		}
+
+		String reversed = sb.toString();
+
+		int[] prefix_normal = computer_prefix(str);
+		int[] prefix_reverse = computer_prefix(reversed);
+
+		for (int i = 0; i <= size; i++) {
+			result[i] = size - prefix_normal[size - 1];
+		}
+
+		for (int ii = 0; ii < size; ii++) {
+			int j = size - prefix_reverse[ii];
+			int k = ii - prefix_reverse[ii] + 1;
+
+			if (result[j] > k) {
+				result[j] = k;
+			}
+		}
+		return result;
 	}
-	
-	*/
-	
-	
-	
+
 	/*
-	 *  Knuth Morris Pratt Algorithm*******************************************
+	 * void prepare_goodsuffix_heuristic(const char *normal, size_t size, int
+	 * result[size + 1]) {
+	 * 
+	 * char *left = (char *) normal; char *right = left + size; char
+	 * reversed[size+1]; char *tmp = reversed + size; size_t i;
+	 * 
+	 * 
+	 * tmp = 0; while (left < right)(--tmp) = *(left++);
+	 * 
+	 * int prefix_normal[size]; int prefix_reversed[size];
+	 * 
+	 * compute_prefix(normal, size, prefix_normal); compute_prefix(reversed,
+	 * size, prefix_reversed);
+	 * 
+	 * for (i = 0; i <= size; i++) { result[i] = size - prefix_normal[size-1]; }
+	 * 
+	 * for (i = 0; i < size; i++) { const int j = size - prefix_reversed[i];
+	 * const int k = i - prefix_reversed[i]+1;
+	 * 
+	 * if (result[j] > k) result[j] = k; } }
+	 */
+
+	/*
+	 * Knuth Morris Pratt Algorithm*******************************************
 	 * algorithm kmp_search: input: an array of characters, S (the text to be
 	 * searched) an array of characters, W (the word sought) output: an integer
 	 * (the zero-based position in S at which W is found)
@@ -358,7 +364,7 @@ public class StrMatch {
 	 * 
 	 * (if we reach here, we have searched all of S unsuccessfully) return
 	 */
-	
+
 	/*
 	 * algorithm kmp_table: input: an array of characters, W (the word to be
 	 * analyzed) an array of integers, T (the table to be filled) output:
@@ -381,9 +387,5 @@ public class StrMatch {
 	 * (third case: we have run out of candidates. Note cnd = 0) otherwise, let
 	 * T[pos] ← 0, pos ← pos + 1
 	 */
-	
-	
-	
-	
 
 }
--- test/StrMatchTest.java
+++ test/StrMatchTest.java
@@ -6,43 +6,47 @@ import java.io.StringReader;
 
 import org.junit.Test;
 
-
 public class StrMatchTest {
-/*
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
-*/
 	/*
-	@Test
-	public void testBruteForce() throws Exception {
-		StrMatch s = new StrMatch();
-		LookBackStringBuffer lbsb = new LookBackStringBuffer("hello world my name is ryan, and i can shoot 3s like no others.");
-		assertEquals(1, s.bruteForce("h", lbsb));
-	}
-	*/
+	 * @Test public void testMain() { fail("Not yet implemented"); }
+	 */
+	/*
+	 * @Test public void testBruteForce() throws Exception { StrMatch s = new
+	 * StrMatch(); LookBackStringBuffer lbsb = newLookBackStringBuffer(
+	 * "hello world my name is ryan, and i can shoot 3s like no others.");
+	 * assertEquals(1, s.bruteForce("h", lbsb)); }
+	 */
 	@Test
 	public void testKMP() throws Exception {
 		StrMatch s = new StrMatch();
-		LookBackStringBuffer lbsb = new LookBackStringBuffer("he\\llo world my name is ryan, but i can't play with black ppl.");
+		LookBackStringBuffer lbsb = new LookBackStringBuffer(
+				"he\\llo world my name is ryan, but i can't play with black ppl.");
 		assertEquals(1, s.knuthMorrisPratt(".", lbsb));
 	}
-	
-//	@Test
-//	public void testSKMP() throws Exception {
-//		StrMatch s = new StrMatch();
-//		assertEquals(1, s.knuthMorrisPrattS("\\l", "hello world my name is ryan, but i can't play with black ppl."));
-//	}
-	
+
+	@Test
+	public void testBM() throws Exception {
+		StrMatch s = new StrMatch();
+		LookBackStringBuffer lbsb = new LookBackStringBuffer(
+		"he\\llo world my name is ryan, but i can't play with black ppl.");
+		assertEquals(1,s.boyerMoore("can't", lbsb));
+	}
+
+	// @Test
+	// public void testSKMP() throws Exception {
+	// StrMatch s = new StrMatch();
+	// assertEquals(1, s.knuthMorrisPrattS("\\l",
+	// "hello world my name is ryan, but i can't play with black ppl."));
+	// }
+
 	@Test
 	public void testRK() throws IOException, Exception {
 		StrMatch s = new StrMatch();
 		LookBackStringBuffer lbsb = new LookBackStringBuffer("hello world");
-//		assertEquals(1, s.rabinKarp("h", lbsb));
-//		assertEquals(-1, s.rabinKarp("naem", lbsb));
-//		assertEquals(1, s.rabinKarp("hello ", lbsb));
-//		assertEquals(1, s.rabinKarp("world", lbsb));
+		// assertEquals(1, s.rabinKarp("h", lbsb));
+		// assertEquals(-1, s.rabinKarp("naem", lbsb));
+		// assertEquals(1, s.rabinKarp("hello ", lbsb));
+		// assertEquals(1, s.rabinKarp("world", lbsb));
 		assertEquals(1, s.rabinKarp("d", lbsb));
 	}
 
