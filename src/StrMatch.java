33c9bfcea53ecf040ddfbc540f1e6bf49882a3f3
jie lian <imjeee@gmail.com>
2011-05-04 15:17:01 -0500

    cleaned up a little

--- src/StrMatch.java
+++ src/StrMatch.java
@@ -6,31 +6,30 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class StrMatch {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		if (args.length != 3) {
 			throw new IllegalArgumentException(
 					"usage: pattern.txt source.txt results.txt");
 		}
 
-		//pattern file parsing
+		// pattern file parsing
 		String pattern = args[0];
 		File p;
 		ArrayList<String> eachPattern = new ArrayList<String>();
 		int largestps = 2048; // longest pattern size
-		
-		//source file parsing
+
+		// source file parsing
 		String source = args[1];
 		FileReader fr = null;
-		
-		//output writer
+
+		// output writer
 		String results = args[2];
 		File outFile = new File(results);
 		StringBuffer output = new StringBuffer();
-		
+
 		try {
 			// read pattern
 			p = new File(pattern);
@@ -59,54 +58,50 @@ public class StrMatch {
 
 			for (String s : eachPattern) {
 
-				
 				System.out.println("*********pattern: ");
-				System.out.println(s);
-				System.out.println();
-				
-				
+				System.out.println(s + "\n");
+
 				lbsb.reset();
 				Stopwatch t = new Stopwatch();
 				String found;
-				
-				//RK rolling linear sum
+
+				// RK rolling linear sum
 				t.start();
 				long[] rk = rabinKarp(s, lbsb);
 				t.stop();
-				
+
 				found = (rk[0] == 1) ? "MATCHED: " : "FAILED: ";
 				output.append("RK " + found + s + "\n");
 				lbsb.reset();
-				
-				System.out.println("rk:  " + rk[0] + " " + rk[1]
-				                      				        + "  " + t.time());
-				
-				//RK rolling base sum
+
+				System.out.println("rk:  " + rk[0] + " " + rk[1] + "  "
+						+ t.time());
+
+				// RK rolling base sum
 				t.start();
 				long[] rk2 = rabinKarp2(s, lbsb);
 				t.stop();
-				
+
 				found = (rk2[0] == 1) ? "MATCHED: " : "FAILED: ";
 				output.append("RK2 " + found + s + "\n");
 				lbsb.reset();
-				
-				System.out.println("rk2:  " + rk2[0] + " " + rk2[1]
-				                      				        + "  " + t.time());
-				
-				//KMP
+
+				System.out.println("rk2:  " + rk2[0] + " " + rk2[1] + "  "
+						+ t.time());
+
+				// KMP
 				t.start();
 				long[] kmp = knuthMorrisPratt(s, lbsb);
 				t.stop();
-				
+
 				found = (kmp[0] == 1) ? "MATCHED: " : "FAILED: ";
 				output.append("KMP " + found + s + "\n");
 				lbsb.reset();
-				
-				System.out.println("KMP: " + kmp[0] + " " + kmp[1] 
-				                        				        + "  " + t.time());
-				
-				
-				//BM
+
+				System.out.println("KMP: " + kmp[0] + " " + kmp[1] + "  "
+						+ t.time());
+
+				// BM
 				t.start();
 				long[] bm = boyerMoore(s, lbsb);
 				t.stop();
@@ -114,23 +109,23 @@ public class StrMatch {
 				found = (bm[0] == 1) ? "MATCHED: " : "FAILED: ";
 				output.append("BM " + found + s + "\n");
 				lbsb.reset();
-				
-				System.out.println("BM:  " + bm[0] + " " 
-						+ bm[1] + "   " + t.time());
-				
-				//Brute Force
+
+				System.out.println("BM:  " + bm[0] + " " + bm[1] + "   "
+						+ t.time());
+
+				// Brute Force
 				t.start();
 				long[] bf = bruteForce(s, lbsb);
 				t.stop();
 
 				found = (bf[0] == 1) ? "MATCHED: " : "FAILED: ";
 				output.append("Brute Force " + found + s + "\n");
-				
-				System.out.println("BF:  " + bf[0] + " " + bf[1]
-				        + "  " + t.time());
+
+				System.out.println("BF:  " + bf[0] + " " + bf[1] + "  "
+						+ t.time());
 			}
-			
-			//write the output buffer to output file
+
+			// write the output buffer to output file
 			FileWriter fw = new FileWriter(outFile);
 			fw.write(output.toString());
 			fw.close();
@@ -174,7 +169,6 @@ public class StrMatch {
 	public static long[] rabinKarp(String pattern, LookBackStringBuffer source)
 			throws IOException, Exception {
 
-		int primeN = 17389;
 		long[] result = new long[3];
 
 		if (!source.hasAvailable(pattern.length())) {
@@ -217,7 +211,7 @@ public class StrMatch {
 				}
 
 			}
-			
+
 			if (!source.hasAvailable(i + 1)) {
 				break;
 			}
@@ -225,23 +219,21 @@ public class StrMatch {
 			char next = source.charAt(i);
 			long nextC = (long) next;
 			long prevHC = source.charAt(i - pattern.length());
-			
+
 			sourceHC = sourceHC - prevHC + nextC;
 		}
 
 		return result;
 	}
-	
+
 	// Rabin-Karp Algorithm, Rolling base sum
 	public static long[] rabinKarp2(String pattern, LookBackStringBuffer source)
 			throws IOException, Exception {
 
 		long p = 17389;
 		long base = 10;
-		long f = fastExp(base, pattern.length()-1, p);
-		
-		
-		
+		long f = fastExp(base, pattern.length() - 1, p);
+
 		long[] result = new long[3];
 
 		if (!source.hasAvailable(pattern.length())) {
@@ -253,16 +245,14 @@ public class StrMatch {
 
 		for (int i = 0; i < pattern.length(); i++) {
 			// calculate the whole pattern hash
-			long patternCharInt = (long)pattern.charAt(i);
+			long patternCharInt = (long) pattern.charAt(i);
 			patternHC = (patternHC * base + patternCharInt) % p;
-			
+
 			// calculate first source has
 			long sourceCharInt = (long) source.charAt(i);
 			sourceHC = (sourceHC * base + sourceCharInt) % p;
 			result[1]++;
 		}
-		
-		
 
 		for (int i = pattern.length(); source.hasAvailable(i + 1); i++) {
 			result[1] += 2;
@@ -286,7 +276,7 @@ public class StrMatch {
 				}
 
 			}
-			
+
 			if (!source.hasAvailable(i + 1)) {
 				break;
 			}
@@ -294,92 +284,54 @@ public class StrMatch {
 			char next = source.charAt(i);
 			long nextC = (long) next;
 			long prevC = source.charAt(i - pattern.length());
-			
-			sourceHC = mod(((sourceHC - prevC*f)* base + nextC),p);
-			
+
+			sourceHC = mod(((sourceHC - prevC * f) * base + nextC), p);
+
 		}
 
 		return result;
 	}
-	
+
 	/*
 	 * fastExp and reverseBits from Ryan's RSA project
 	 */
-	private static long fastExp (long num, long e, long n){
+	private static long fastExp(long num, long e, long n) {
 		e = reverseBits(e);
 		int skipped = 0;
-		while ((e & 1l) != 1){
+		while ((e & 1l) != 1) {
 			skipped++;
 			e >>= 1;
 		}
 		long c = 1;
-		for(int remaining = 64 - skipped; remaining > 0; remaining--){
+		for (int remaining = 64 - skipped; remaining > 0; remaining--) {
 			if ((e & 1) == 0) {
-				c = ((c*c) % n);
-			}
-			else {
-				c = ((((c*c) % n) * num) % n);
+				c = ((c * c) % n);
+			} else {
+				c = ((((c * c) % n) * num) % n);
 			}
 			e >>>= 1;
-		} 
+		}
 		return c;
 	}
-	private static long reverseBits(long e){
-		e = ((e >>> 1) & 0x5555555555555555l) | ((e & 0x5555555555555555l) << 1);
-		e = ((e >>> 2) & 0x3333333333333333l) | ((e & 0x3333333333333333l) << 2);
-		e = ((e >>> 4) & 0x0F0F0F0F0F0F0F0Fl) | ((e & 0x0F0F0F0F0F0F0F0Fl) << 4);
-		e = ((e >>> 8) & 0x00FF00FF00FF00FFl) | ((e & 0x00FF00FF00FF00FFl) << 8);
-		e = ((e >>> 16) & 0x0000FFFF0000FFFFl) | ((e & 0x0000FFFF0000FFFFl) << 16);
+
+	private static long reverseBits(long e) {
+		e = ((e >>> 1) & 0x5555555555555555l)
+				| ((e & 0x5555555555555555l) << 1);
+		e = ((e >>> 2) & 0x3333333333333333l)
+				| ((e & 0x3333333333333333l) << 2);
+		e = ((e >>> 4) & 0x0F0F0F0F0F0F0F0Fl)
+				| ((e & 0x0F0F0F0F0F0F0F0Fl) << 4);
+		e = ((e >>> 8) & 0x00FF00FF00FF00FFl)
+				| ((e & 0x00FF00FF00FF00FFl) << 8);
+		e = ((e >>> 16) & 0x0000FFFF0000FFFFl)
+				| ((e & 0x0000FFFF0000FFFFl) << 16);
 		e = (e >>> 32) | (e << 32);
 		return e;
 	}
-	
-	
-	
-	public static long mod(long n, long x){
-		return (n >= 0) ? (n % x) : ((n -(n/x - 1)*x)%x);
-	}
 
-	/*
-	 * // Rabin-Karp Algorithm public static int rabinKarp(String pattern,
-	 * String source) throws IOException, Exception { assert pattern.length() <
-	 * source.length() : "pattern needs to be longer than source";
-	 * 
-	 * String sub = new String();
-	 * 
-	 * int primeN = 47; int pl = pattern.length();
-	 * 
-	 * long patternHC = 0; long sourceHC = 0;
-	 * 
-	 * for(int i = 0; i < pattern.length(); i++){ //calculate the whole pattern
-	 * hash long patternCharInt = pattern.charAt(i); patternHC +=
-	 * patternCharInt; //calculate first source has char sourceChar =
-	 * source.charAt(i); long sourceCharInt = (long) sourceChar; sourceHC +=
-	 * sourceCharInt;
-	 * 
-	 * sub += sourceChar; }
-	 * 
-	 * //System.out.println("pattern: " + patternHC + " source: " + sourceHC);
-	 * 
-	 * for(int i = pattern.length()-1; i < source.length(); i++){
-	 * //System.out.println("sourceHC " + sourceHC); if(patternHC == sourceHC){
-	 * if(bruteForce(pattern, new LookBackStringBuffer(sub)) != -1){ return 1; }
-	 * } //System.out.println(source.charAt(i));
-	 * 
-	 * if(i+1 >= source.length()){ break; }
-	 * 
-	 * char next = source.charAt(i+1); long nextC = (long)next; sub =
-	 * sub.substring(1, sub.length()) + next; long prevHC = source.charAt(i -
-	 * pattern.length() + 1); sourceHC = sourceHC - prevHC + nextC;
-	 * 
-	 * //System.out.println("prev: " + prevHC);
-	 * 
-	 * 
-	 * //System.out.println("next: " + nextC + " source: " + sourceHC); }
-	 * 
-	 * 
-	 * return -1; }
-	 */
+	public static long mod(long n, long x) {
+		return (n >= 0) ? (n % x) : ((n - (n / x - 1) * x) % x);
+	}
 
 	// Knuth-Morris-Pratt Algorithm
 	public static long[] knuthMorrisPratt(String pattern,
@@ -394,13 +346,13 @@ public class StrMatch {
 		int m = 0;
 		int i = 0;
 		int[] T = partialMatchTable(w);
-		result[1] += w.length()*2;
-		
-//		for (int ii = 0; ii < T.length; ii++) {
-//			System.out.print(T[ii]);
-//		}
-//		System.out.println();
-		
+		result[1] += w.length() * 2;
+
+		// for (int ii = 0; ii < T.length; ii++) {
+		// System.out.print(T[ii]);
+		// }
+		// System.out.println();
+
 		while (s.hasAvailable(m + i + 1)) {
 
 			result[1] += 2;
@@ -474,7 +426,7 @@ public class StrMatch {
 				j--;
 			}
 			if (j > 0) {
-				int x = ((int) source.charAt(s+j-1)) & 255;
+				int x = ((int) source.charAt(s + j - 1)) & 255;
 				int k = badcharacter[x];
 				int m = j - k - 1;
 				if (k < j & m > goodSuffix[j]) {
@@ -566,45 +518,5 @@ public class StrMatch {
 		}
 		return result;
 	}
-
-	/*
-	 * Knuth Morris Pratt Algorithm*******************************************
-	 * algorithm kmp_search: input: an array of characters, S (the text to be
-	 * searched) an array of characters, W (the word sought) output: an integer
-	 * (the zero-based position in S at which W is found)
-	 * 
-	 * define variables: an integer, m ← 0 (the beginning of the current match
-	 * in S) an integer, i ← 0 (the position of the current character in W) an
-	 * array of integers, T (the table, computed elsewhere)
-	 * 
-	 * while m+i is less than the length of S, do: if W[i] = S[m + i], if i
-	 * equals the (length of W)-1, return m let i ← i + 1 otherwise, let m ← m +
-	 * i - T[i], if T[i] is greater than -1, let i ← T[i] else let i ← 0
-	 * 
-	 * (if we reach here, we have searched all of S unsuccessfully) return
-	 */
-
-	/*
-	 * algorithm kmp_table: input: an array of characters, W (the word to be
-	 * analyzed) an array of integers, T (the table to be filled) output:
-	 * nothing (but during operation, it populates the table)
-	 * 
-	 * define variables: an integer, pos ← 2 (the current position we are
-	 * computing in T) an integer, cnd ← 0 (the zero-based index in W of the
-	 * next character of the current candidate substring)
-	 * 
-	 * (the first few values are fixed but different from what the algorithm
-	 * might suggest) let T[0] ← -1, T[1] ← 0
-	 * 
-	 * while pos is less than the length of W, do: (first case: the substring
-	 * continues) if W[pos - 1] = W[cnd], let cnd ← cnd + 1, T[pos] ← cnd, pos ←
-	 * pos + 1
-	 * 
-	 * (second case: it doesn't, but we can fall back) otherwise, if cnd > 0,
-	 * let cnd ← T[cnd]
-	 * 
-	 * (third case: we have run out of candidates. Note cnd = 0) otherwise, let
-	 * T[pos] ← 0, pos ← pos + 1
-	 */
 
 }
